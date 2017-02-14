eefdb869817b1116651a0bb09a4490d72a9837ab
codelulu <codeululu@qq.com>
2017-02-14 11:45:24 +0800

    adjust stack implementation

--- introduction_to_algorithms/data_structure/stack/stack.py
+++ introduction_to_algorithms/data_structure/stack/stack.py
@@ -3,38 +3,45 @@
 
 class Stack:
 
-    def __init__(self):
-        self.S = []
+    N = 5
 
-    def top(self):
-        return len(self.S)
+    def __init__(self):
+        self.S = [None] * self.N
+        self.top = 0
 
     def STACK_EMPTY(self):
-        if self.top() == 0:
+        if self.top == 0:
             return True
         else:
             return False
 
     def PUSH(self, x):
-        self.S.append(x)
+        if self.top == self.N:
+            raise Exception('overflow')
+
+        self.S[self.top] = x
+        self.top += 1
 
     def POP(self):
         if self.STACK_EMPTY():
             raise Exception('underflow')
-        return self.S.pop(self.top() - 1)
+
+        x = self.S[self.top - 1]
+        self.top -= 1
+        return x
 
     def __str__(self):
-        return str(self.S)
+        return 'S = %s, top = %d, N = %d' % (str(self.S), self.top, self.N)
 
 
 if __name__ == '__main__':
     S = Stack()
 
-    print 'S = %s' % S
+    print '%s' % S
     print 'S.STACK_EMPTY = %s' % S.STACK_EMPTY()
     S.PUSH(1.2)
     S.PUSH(45)
-    print 'S = %s' % S
+    print '%s' % S
     print 'S.POP = %s' % S.POP()
     print 'S.POP = %s' % S.POP()
 
